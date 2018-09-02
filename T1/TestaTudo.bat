1d34e6eed08c559bb0e70fa837f42b7cb3173728
Nicholas G <nicholasgodoyx@gmail.com>
2018-09-02 19:30:27 -0300

    script de testes gerais

--- T1/TestaTudo.bat
+++ T1/TestaTudo.bat
@@ -2,5 +2,6 @@ del estatisticas.estat
 
 TRAB1-1      /sTesteBasicoApenasMultArvores        /lTesteBasicoApenasMultArvores        /aestatisticas
 TRAB1-1      /sTesteBasicoApenasExibirArvore       /lTesteBasicoApenasExibirArvore       /aestatisticas
+TRAB1-1      /sTestaTudo      /lTestaTudo     /aestatisticas
 
 exbestat /eestatisticas
