995304f62b36deed2a5f432c31de38def19e5d62
Nicholas G <nicholasgodoyx@gmail.com>
2018-09-02 18:16:50 -0300

    adicionar arquivos do trabalho

---
+++ T1/TestaTudo.bat
@@ -0,0 +1,6 @@
+del estatisticas.estat
+
+TRAB1-1      /sTesteBasicoApenasMultArvores        /lTesteBasicoApenasMultArvores        /aestatisticas
+TRAB1-1      /sTesteBasicoApenasExibirArvore       /lTesteBasicoApenasExibirArvore       /aestatisticas
+
+exbestat /eestatisticas
