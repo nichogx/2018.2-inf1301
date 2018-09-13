5f71d015eeb2b4887fb70fa85bb5800e94de4c5b
Nicholas G <nicholasgodoyx@gmail.com>
2018-09-13 13:29:40 -0300

    T2 - Inicio do Desenvolvimento

---
+++ T2/GRAFO.C
@@ -0,0 +1,110 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo Grafo
+*
+*  Arquivo:                 GRAFO.C
+*  Letras identificadoras:  GRF
+*
+*  Projeto: Disciplina INF1301
+*  Autores: ngx - Nicholas Godoy
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*       0.10   ngx   13/09/2018 Início do desenvolvimento
+*
+***************************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define GRAFO_OWN
+#include "GRAFO.H"
+#undef GRAFO_OWN
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: GRF Descritor do nó do grafo
+*
+*
+*  $ED Descrição do tipo
+*     Descreve a organização do nó
+*
+***********************************************************************/
+
+typedef struct tgNoGrafo tpNoGrafo;
+struct tgNoGrafo {
+	char valor; /* valor do nó */
+};
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: GRF Descritor da cabeça de um grafo
+*
+*
+*  $ED Descrição do tipo
+*     A cabeça do grafo é o ponto de acesso para um determinado grafo.
+*
+***********************************************************************/
+
+typedef struct tgGrafo tpGrafo;
+struct tgGrafo {
+	tpNoGrafo *pNoCorr; /* ponteiro para o nó corrente do grafo */
+};
+
+/*****  Dados encapsulados no módulo  *****/
+
+static tpGrafo *pGrafo = NULL;
+
+/***** Protótipos das funções encapuladas no módulo *****/
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+/***************************************************************************
+*
+*  Função: GRF Criar grafo
+*  ****/
+
+GRF_tpCondRet GRF_CriarGrafo()
+{
+	if (pGrafo != NULL)
+		return GRF_CondRetGrafoJaExiste;
+
+	pGrafo = (tpGrafo *) malloc(sizeof(tpGrafo));
+	if (pGrafo == NULL)
+		return GRF_CondRetFaltouMemoria;
+
+	pGrafo->pNoCorr = NULL;
+
+	return GRF_CondRetOK;
+} /* fim função: GRF Criar Grafo */
+
+/***************************************************************************
+*
+*  Função: ARV Destruir árvore
+*  ****/
+
+GRF_tpCondRet GRF_DestruirGrafo(void)
+{
+	if (pGrafo == NULL)
+		return GRF_CondRetGrafoNaoExiste;
+
+	// TODO continuar
+}
+
+   void ARV_DestruirArvore( void )
+   {
+
+      if ( pArvore != NULL )
+      {
+         if ( pArvore->pNoRaiz != NULL )
+         {
+            DestroiArvore( pArvore->pNoRaiz ) ;
+         } /* if */
+         free( pArvore ) ;
+         pArvore = NULL ;
+      } /* if */
+
+   } /* Fim função: ARV Destruir árvore */
+
+/*****  Código das funções encapsuladas no módulo  *****/
+
+/********** Fim do módulo de implementação: Módulo Grafo **********/
---
+++ T2/TESTGRF.C
@@ -0,0 +1,92 @@
+/***************************************************************************
+*  $MCI Módulo de implementação: Módulo de teste específico
+*
+*  Arquivo:                  TESTGRF.C
+*  Letras identificadoras:   TGRF
+*
+*  Nome da base de software:    Exemplo de teste automatizado
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\SIMPLES.BSW
+*
+*  Projeto: Disciplina 1301
+*  Autores: ngx - Nicholas Godoy
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*       0.10   ngx   13/09/2018 Início do desenvolvimento
+*
+*  $ED Descrição do módulo
+*     Este módulo contém as funções específicas para o teste do
+*     módulo grafo.
+*
+*  $EIU Interface com o usuário pessoa
+*     Comandos de teste específicos para testar o módulo árvore:
+*
+*     "=criar"      - chama a função GRF_CriarGrafo( )
+*
+***************************************************************************/
+
+#include    <string.h>
+#include    <stdio.h>
+
+#include    "TST_ESPC.H"
+
+#include    "generico.h"
+#include    "lerparm.h"
+
+#include    "grafo.h"
+
+/* Tabela dos nomes dos comandos de teste específicos */
+
+#define     CRIAR_GRF_CMD       "=criar"
+
+/*****  Código das funções exportadas pelo módulo  *****/
+
+
+/***********************************************************************
+*
+*  $FC Função: TGRF Efetuar operações de teste específicas para árvore
+*
+*  $ED Descrição da função
+*     Efetua os diversos comandos de teste específicos para o módulo
+*     grafo sendo testado.
+*
+*  $EP Parâmetros
+*     $P ComandoTeste - String contendo o comando
+*
+*  $FV Valor retornado
+*     Ver TST_tpCondRet definido em TST_ESPC.H
+*
+***********************************************************************/
+
+TST_tpCondRet TST_EfetuarComando( char * ComandoTeste )
+{
+	GRF_tpCondRet CondRetObtido   = GRF_CondRetOK;
+	GRF_tpCondRet CondRetEsperada = GRF_CondRetFaltouMemoria;
+                                      /* inicializa para qualquer coisa */
+	char ValorEsperado = '?';
+	char ValorObtido   = '!';
+	char ValorDado     = '\0';
+
+	int  NumLidos = -1;
+
+	TST_tpCondRet Ret;
+
+	/* Testar GRF Criar grafo */
+
+	if (strcmp(ComandoTeste, CRIAR_GRF_CMD) == 0) {
+		NumLidos = LER_LerParametros("i", &CondRetEsperada);
+		if (NumLidos != 1)
+			return TST_CondRetParm;
+
+		CondRetObtido = GRF_CriarGrafo();
+
+		return TST_CompararInt(CondRetEsperada, CondRetObtido, 
+				       "Retorno errado ao criar grafo.");
+	}
+
+	return TST_CondRetNaoConhec;
+
+   } /* Fim função: TGRF Efetuar operações de teste específicas para grafo */
+
+/********** Fim do módulo de implementação: Módulo de teste específico **********/
+
