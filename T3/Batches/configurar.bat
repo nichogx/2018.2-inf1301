d8b6238cf415780f677d87f3ce31b97e186c0c2a
Nicholas G <nicholasgodoyx@gmail.com>
2018-11-11 13:40:01 -0200

    bats, leia-me e automato exemplo

---
+++ T3/Batches/configurar.bat
@@ -0,0 +1,4 @@
+if exist .\LEX.bat.txt ren .\LEX.bat.txt LEX.bat
+if exist ..\Objetos\ArcaboucoTeste.lib.txt ren ..\Objetos\ArcaboucoTeste.lib.txt ArcaboucoTeste.lib
+if exist ..\Objetos\MinhaBiblioteca.lib.txt ren ..\Objetos\MinhaBiblioteca.lib.txt MinhaBiblioteca.lib
+if exist ..\Objetos\TRAB3-1.exe.txt ren ..\Objetos\TRAB3-1.exe.txt TRAB3-1.exe
