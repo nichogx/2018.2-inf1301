c007839c525891d6649768d0d2f63cf4ace35149
Nicholas G <nicholasgodoyx@gmail.com>
2018-12-02 13:19:18 -0200

    scripts e bats

--- T4/Batches/TestaTudo.bat
+++ T4/Batches/TestaTudo.bat
@@ -1,2 +1,5 @@
+del ..\scripts\*_acum.cont
+
 ..\produto\TRAB4-1        /s..\scripts\testelista    /l..\produto\testelista
 ..\produto\TRAB4-2        /s..\scripts\testelistadebug    /l..\produto\testelistadebug
+..\produto\TRAB4-2        /s..\scripts\testelistadeturpacoes    /l..\produto\testelistadeturpacoes
