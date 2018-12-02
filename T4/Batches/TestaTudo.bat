d7edbf4f8c9587e00d1574bcb7bb1715dd24dd28
Nicholas G <nicholasgodoyx@gmail.com>
2018-12-02 13:39:12 -0200

    bat atualizado, script e cont

--- T4/Batches/TestaTudo.bat
+++ T4/Batches/TestaTudo.bat
@@ -3,3 +3,5 @@ del ..\scripts\*_acum.cont
 ..\produto\TRAB4-1        /s..\scripts\testelista    /l..\produto\testelista
 ..\produto\TRAB4-2        /s..\scripts\testelistadebug    /l..\produto\testelistadebug
 ..\produto\TRAB4-2        /s..\scripts\testelistadeturpacoes    /l..\produto\testelistadeturpacoes
+
+type ..\scripts\todos_acum.cont
